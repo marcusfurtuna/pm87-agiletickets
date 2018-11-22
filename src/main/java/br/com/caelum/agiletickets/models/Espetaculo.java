0bb20c67f2483fb2625ecb21d379d3a0951d9a18
Marcus Furtuna <marcus.furtuna@hotmail.com>
2018-11-22 11:24:15 -0200

    Atualizando projeto

--- src/main/java/br/com/caelum/agiletickets/models/Espetaculo.java
+++ src/main/java/br/com/caelum/agiletickets/models/Espetaculo.java
@@ -13,6 +13,8 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import org.joda.time.DateTime;
+import org.joda.time.Days;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
 
@@ -97,9 +99,8 @@ public class Espetaculo {
      * Repare que a data da primeira sessao é sempre a data inicial.
      */
 	public List<Sessao> criaSessoes(LocalDate inicio, LocalDate fim, LocalTime horario, Periodicidade periodicidade) {
-		// ALUNO: Não apague esse metodo. Esse sim será usado no futuro! ;)
-		return null;
-	}
+		return periodicidade.getCriadorDeSessoes().criaSessoes(inicio, fim, horario, this);
+}
 	
 	public boolean Vagas(int qtd, int min)
    {
--- src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
+++ src/main/java/br/com/caelum/agiletickets/models/TipoDeEspetaculo.java
@@ -1,7 +1,23 @@
 package br.com.caelum.agiletickets.models;
 
+import br.com.caelum.agiletickets.domain.precos.CalculadorDePreco;
+
 public enum TipoDeEspetaculo {
 	
-	CINEMA, SHOW, TEATRO, BALLET, ORQUESTRA;
+	CINEMA(new CalculadorDePrecoCinema()), 
+	SHOW(new CalculadorDePrecoShow()), 
+	TEATRO(new CalculadorDePrecoTeatro()),
+	BALLET(new CalculadorDePrecoBallet()),
+	ORQUESTRA(new CalculadorDePrecoOrquestra());
+	
+	private final CalculadorDePreco calculadorDePreco;
+	
+	private TipoDeEspetaculo(CalculadorDePreco calculadorDePreco) {
+		this.calculadorDePreco = calculadorDePreco;
+	}
+	
+	public CalculadorDePreco getCalculadorDePreco() {
+		return this.calculadorDePreco;
+	}
 	
 }
