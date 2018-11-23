e4c7bb44c23c3ffff0b14657951f1df2e037f236
Marcus Furtuna <marcus.furtuna@hotmail.com>
2018-11-23 08:30:01 -0200

    Atualizando projeto com arquivos novos

---
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -0,0 +1,16 @@
+package br.com.caelum.agiletickets.domain.precos;
+
+import java.math.BigDecimal;
+
+import br.com.caelum.agiletickets.models.Sessao;
+
+public class CalculadoraDePrecos {
+
+	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
+		CalculadorDePreco calculador = sessao.getEspetaculo().getTipo().getCalculadorDePreco();
+		BigDecimal precoUnitario = calculador.calcula(sessao);
+		
+		return precoUnitario.multiply(BigDecimal.valueOf(quantidade));
+	}
+
+}
